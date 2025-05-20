85eb2e73e70e3b781c1a2d3e408d6c429a1e636a
Vitor-Alves17 <vitoralves3306@gmail.com>
2025-05-20 10:53:47 -0300

    update

---
+++ src/Aula10/Grimorio/Grimo.java
@@ -0,0 +1,19 @@
+package Aula10.Grimorio;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Grimo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Olá, seja bem vindo ao Grimorio-Digital-2000! ");
+        System.out.println("Comece adicionando seus feitiços: ");
+        boolean ver = true;
+        String ver2 = "s";
+        ArrayList<String> grimos = new ArrayList<>();
+
+    }
+}
---
+++ src/Aula10/PedraPapelTe/Jokepo.java
@@ -0,0 +1,4 @@
+package Aula10.PedraPapelTe;
+
+public class Jokepo {
+}

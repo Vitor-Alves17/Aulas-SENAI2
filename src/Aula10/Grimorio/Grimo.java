9a2293c46dd32c0a9c0271d955eb14cd68d563bf
Vitor-Alves17 <vitoralves3306@gmail.com>
2025-05-21 08:31:36 -0300

    update

--- src/Aula10/Grimorio/Grimo.java
+++ src/Aula10/Grimorio/Grimo.java
@@ -15,5 +15,27 @@ public class Grimo {
         String ver2 = "s";
         ArrayList<String> grimos = new ArrayList<>();
 
+        do {
+            grimos.add(sc.nextLine());
+            System.out.println("Deseja adicionar um novo feitiço? (s/n): ");
+            ver2 = sc.nextLine();
+            while(!ver2.equals("s") && !ver2.equals("n")) {
+                System.out.println("Digito inválido, digite novamente: ");
+                ver2 = sc.nextLine();
+            }
+            if (ver2.equals("n")) {
+                ver = false;
+            }
+            if(ver == true) {
+                System.out.println("Adicione o proximo feitiço: ");
+            }
+        }while(ver == true);
+        //System.out.println(grimos);
+
+        System.out.println("Agora que seu grimório está completo, você deseja buscar algum de seus feitiços? (s/n): ");
+        ver2 = sc.nextLine();
+        while (!ver2.equals("s") && !ver2.equals("n")) {
+
+        }
     }
 }
